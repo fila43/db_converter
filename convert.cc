e28198219473e9787b56154ae11759491736d95e
Filip Janus <fjanus@redhat.com>
2023-07-07 09:19:36 +0200

    Use GDBM as default example

--- convert.cc
+++ convert.cc
@@ -3,12 +3,10 @@
 int main(int argc, char* argv[]){
 	Libdb x =Libdb();
 	x.connect_database("access.db");
-	LMDB_ y = LMDB_();
-	y.create_database("./");
-	y.fill_database(x);
-	y.close_db();
 	GDBM_ b = GDBM_();
-	b.create_database("test.gdbm");
-	//b.fill_database(x);
+	b.create_database("access.gdbm");
+	if (!b.fill_database(x))
+		return 1;
+    	b.close_db();
 	return 0;
 }
